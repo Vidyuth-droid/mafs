d43f1d0ddb7d53533e69c6afee9a7d2326996e2f
Vidyuth-droid <72655577+Vidyuth-droid@users.noreply.github.com>
2021-05-08 15:48:44 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -9,32 +9,12 @@ document.getElementById("player2_score").innerHTML=player2_score;
 document.getElementById("player_question").innerHTML="Question turn - "+player1_name;
 document.getElementById("player_answer").innerHTML="Answer turn - "+player2_name;
 function send(){
-    get_word=document.getElementById("word").value;
-    word=get_word.toLowerCase();
-    console.log("equation = "+word);
+number1 = document.getElementById("No_1").value;
+number2 = document.getElementById("No_2").value;
+actualnumber = parseInt(number1)*parseInt(number2);
+question_number = "<h4>"+ number1+ " * "+ number2+"</h4>"
+input_box = 
 
-    charAt1=word.charAt(1);
-    console.log(charAt1);
-
-    length_divide_2=Math.floor(word.length/2);
-    charAt2=word.charAt(length_divide_2);
-    console.log(charAt2);
-
-    length_minus_1=word.length-1;
-    charAt3=word.charAt(length_minus_1);
-    console.log(charAt3);
-
-    remove_charAt1=word.replace(charAt1,"_");
-    remove_charAt2=remove_charAt1.replace(charAt2,"_");
-    remove_charAt3=remove_charAt2.replace(charAt3,"_");
-    console.log(remove_charAt3);
-
-    question_word="<h4 id='word_display'>Q. "+remove_charAt3+"</h4>";
-    input_box="<br> Answer : <input type='text' id='input_check_box'>";
-    check_button="<br> <br> <button class='btn btn-info' onclick='check()'>Check</button>";
-    row=question_word+input_box+check_button;
-    document.getElementById("output").innerHTML=row;
-    document.getElementById("word").value="";
 }
 question_turn="player1";
 answer_turn="player2";
